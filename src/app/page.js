463eeb0e74d519c13ce5f839350e91d33cddf8b5
souzatotolo <totolo.adv@gmail.com>
2024-01-17 23:26:30 -0800

    added to the top button

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,8 @@ import { Header } from '@/components/Header';
 import { Introduction } from './pages/Introduction';
 import { About } from './pages/About';
 import { Projects } from './pages/Projects';
-import { Motion } from './pages/Motion';
 import { Example } from '@/components/Example';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { mainRoute } from '@/routes/mainRoute';
 import GoToTop from '@/components/goToTheTop';
 
 export default function Home() {
